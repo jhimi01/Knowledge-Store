d3d997a467dc84df0dffedbcfcfb7faf027b60e5
Jhimi Talukder <talukderjhimi@gmail.com>
2023-04-12 20:02:12 +0600

    filter done

--- src/component/AppliedJob.jsx
+++ src/component/AppliedJob.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
-import { useLoaderData } from 'react-router-dom';
+import { NavLink, useLoaderData } from 'react-router-dom';
 import { getjobdetails } from '../utilities/fakedb';
 import ReviewJob from './ReviewJob';
 
@@ -32,17 +32,23 @@ const AppliedJob = () => {
 
 
         {/* dropdown dropdown-end */}
-        <div className="dropdown dropdown-end">
-  <label tabIndex={0} className="btn m-1">filter</label>
-  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-  <button onClick={()=>Remotehandler('Remote')}>Remote</button>
-  <button onClick={()=>Outsiehandler('Outsie')}>Outsie</button>
-  <button onClick={()=>setRemote(appliedjobs)}>All</button>
+        <div className="dropdown dropdown-end my-4 ">
+ <div className='flex items-center flex-row bg-gray-200 py-3 px-4 m-1 rounded'>
+ <label tabIndex={0} className=" text-gray-600 font-bold cursor-pointer rounded ">filter
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+</svg>
+  </label>
+ </div>
+  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded w-52">
+  <button className='hover:bg-slate-300 rounded text-gray-700 font-semibold' onClick={()=>Remotehandler('Remote')}>Remote</button>
+  <button className='hover:bg-slate-300 rounded text-gray-700 font-semibold' onClick={()=>Outsiehandler('Outsie')}>Outsie</button>
+  <button className='hover:bg-slate-300 rounded text-gray-700 font-semibold' onClick={()=>setRemote(appliedjobs)}>All</button>
   </ul>
 </div>
 </div>
 </div>
-          <div className='mt-20'>
+          <div className='mt-28'>
           {
               
               localStorage.getItem('applied-jobs')?remote.map(job => <ReviewJob job={job} key={job.id}></ReviewJob>):<h2>You didn't applied any job</h2>
