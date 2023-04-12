518d7fea223fc70ee32e8f1d5416470f6a4580d7
Jhimi Talukder <talukderjhimi@gmail.com>
2023-04-12 19:43:22 +0600

    filter remote and outside

--- src/component/AppliedJob.jsx
+++ src/component/AppliedJob.jsx
@@ -7,28 +7,22 @@ import ReviewJob from './ReviewJob';
 const AppliedJob = () => {
     // // load data
     const data = useLoaderData()
-    // console.log(data)
-   
+  
     // get fob details form localstorage 
     const appliedjobs = getjobdetails(data)
-    console.log(appliedjobs)
 
-    const [remote, setRemote] = useState({});
-    useEffect(()=>{
-      const filteremote = appliedjobs.filter(item => item.remote_partTIme === 'Remote')
+    const [remote, setRemote] = useState(appliedjobs);
+    
+    const Remotehandler = (Remote) =>{
+      const filteremote = appliedjobs.filter(item => item.remote_partTIme === Remote)
       setRemote(filteremote)
-    },[])
-
-    console.log(remote)
-
-
-    // filter value
-    const filtervalueremote = (filteremote)=>{
-          
     }
 
+    const Outsiehandler =(Outsie)=>{
+      const filteremote = appliedjobs.filter(item => item.remote_partTIme === Outsie)
+      setRemote(filteremote)
+    }
 
-   
 
     return (
         <div className='pt-4 px-7 relative'>
@@ -37,19 +31,21 @@ const AppliedJob = () => {
         <div className="dropdown dropdown-bottom dropdown-end">
 
 
-  <label tabIndex={0} className="btn m-1">Filter</label>
+        {/* dropdown dropdown-end */}
+        <div className="dropdown dropdown-end">
+  <label tabIndex={0} className="btn m-1">filter</label>
   <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-  {/* <button onClick={()=>handlesortremote()}>remote</button>
-  <button onClick={()=>handlesortOutside()}>Outside</button> */}
-    <li onClick={()=>filtervalueremote()}><a>Remote</a></li>
-    <li><a>Outside</a></li>
+  <button onClick={()=>Remotehandler('Remote')}>Remote</button>
+  <button onClick={()=>Outsiehandler('Outsie')}>Outsie</button>
+  <button onClick={()=>setRemote(appliedjobs)}>All</button>
   </ul>
+</div>
 </div>
 </div>
           <div className='mt-20'>
           {
               
-              localStorage.getItem('applied-jobs')?appliedjobs.map(job => <ReviewJob filtervalueremote={filtervalueremote} job={job} key={job.id}></ReviewJob>):<h2>You didn't applied any job</h2>
+              localStorage.getItem('applied-jobs')?remote.map(job => <ReviewJob job={job} key={job.id}></ReviewJob>):<h2>You didn't applied any job</h2>
           }
           </div>
         </div>
